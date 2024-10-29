6cc49952b96129b80643d0361767d3b0621b0f6e
= <=>
2024-10-29 21:33:35 +0530

    Category Service

--- src/main/java/com/dreamshops/service/category/CategoryService.java
+++ src/main/java/com/dreamshops/service/category/CategoryService.java
@@ -1,5 +1,21 @@
 package com.dreamshops.service.category;
 
+import com.dreamshops.entity.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    Category getCategoryById(Long categoryId);
+
+    Category getCategoryByName(String categoryName);
+
+    List<Category> getAllCategory();
+
+    Category addCategory(Category category);
+
+    Category updateCategory(Category category, Long categoryId);
+
+    void deleteCategory(String categoryId);
 
 }
--- src/main/java/com/dreamshops/service/category/CategoryServiceImpl.java
+++ src/main/java/com/dreamshops/service/category/CategoryServiceImpl.java
@@ -1,5 +1,47 @@
 package com.dreamshops.service.category;
 
-public class CategoryServiceImpl {
+import com.dreamshops.entity.Category;
+import com.dreamshops.exception.ResourceNotFoundException;
+import com.dreamshops.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Category getCategoryById(Long categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(()->new ResourceNotFoundException("Category not found with id: "+categoryId));
+    }
+
+    @Override
+    public Category getCategoryByName(String categoryName) {
+        return categoryRepository.findByName(categoryName);
+    }
+
+    @Override
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(String categoryId) {
+
+    }
 }
