e10972329351554230eb8290beadddc2bf2c5b80
Vishwakarma-amit <Vishwakarma.amit@outlook.in>
2024-10-30 14:00:13 +0530

    Image service

--- src/main/java/com/dreamshops/service/image/ImageService.java
+++ src/main/java/com/dreamshops/service/image/ImageService.java
@@ -1,5 +1,21 @@
 package com.dreamshops.service.image;
 
+import com.dreamshops.dto.ImageDto;
+import com.dreamshops.entity.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface ImageService {
+
+    Image getImageByUrl(String downloadUrl);
+
+    Image getImageById(Long imageId);
+
+    void deleteImageById(Long imageId);
+
+    List<ImageDto> saveImage(List<MultipartFile> file, Long productId);
+
+    Image updateImage(MultipartFile file, Long imageId);
 
 }
--- src/main/java/com/dreamshops/service/image/ImageServiceImpl.java
+++ src/main/java/com/dreamshops/service/image/ImageServiceImpl.java
@@ -1,8 +1,94 @@
 package com.dreamshops.service.image;
 
+import com.dreamshops.dto.ImageDto;
+import com.dreamshops.entity.Image;
+import com.dreamshops.entity.Product;
+import com.dreamshops.exception.ResourceNotFoundException;
+import com.dreamshops.repository.ImageRepository;
+import com.dreamshops.service.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
-public class ImageServiceImpl {
+public class ImageServiceImpl implements ImageService {
 
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @Override
+    public Image getImageByUrl(String downloadUrl) {
+        return imageRepository.findByDownloadUrl(downloadUrl);
+    }
+
+    @Override
+    public Image getImageById(Long imageId) {
+        return imageRepository.findById(imageId)
+                .orElseThrow(()-> new ResourceNotFoundException("Image not found with id: "+imageId));
+    }
+
+    @Override
+    public void deleteImageById(Long imageId) {
+        imageRepository.findById(imageId)
+                .ifPresentOrElse(imageRepository::delete,()->{
+                    throw new ResourceNotFoundException("Image not found with id: "+imageId);
+                });
+    }
+
+    @Override
+    public List<ImageDto> saveImage(List<MultipartFile> files, Long productId) {
+        Product product = productService.getProductById(productId);
+        List<ImageDto> imageDtos = new ArrayList<>();
+        for(MultipartFile file: files){
+            try{
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                String buildDownloadUrl = "/api/v1/images/image/download/";
+
+                Image savedImage = imageRepository.save(image);
+                savedImage.setDownloadUrl(buildDownloadUrl+savedImage.getImageId());
+                imageRepository.save(image);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setImageId(savedImage.getImageId());
+                imageDto.setFileName(savedImage.getFileName());
+                imageDto.setFileType(savedImage.getFileType());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+
+                imageDtos.add(imageDto);
+
+            } catch (IOException | SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return imageDtos;
+    }
+
+    @Override
+    public Image updateImage(MultipartFile file, Long imageId) {
+        Image image = imageRepository.findById(imageId)
+                .orElseThrow(()-> new ResourceNotFoundException("Image not found with id: "+imageId));
+        try {
+            image.setFileName(file.getOriginalFilename());
+            image.setFileType(file.getContentType());
+            image.setImage(new SerialBlob(file.getBytes()));
+            return imageRepository.save(image);
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
